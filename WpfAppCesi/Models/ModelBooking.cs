c545a2389e196991776dbfaf6b20abed618d5d6b
ADRIEN MARINI <adrien.marini@live.fr>
2018-10-28 15:37:19 +0100

    ajout des fonctions de manipulation des données dans le model pour les autres entités

--- WpfAppCesi/Models/ModelBooking.cs
+++ WpfAppCesi/Models/ModelBooking.cs
@@ -16,7 +16,7 @@ namespace WpfAppCesi
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder) { }
 
-        #region Méthodes Hotel
+        #region Méthodes Hotels
         public bool SupprimerHotel(HotelsSet hotel)
         {
             try
@@ -82,6 +82,236 @@ namespace WpfAppCesi
                 if (idHotel == 0)   //nouvel hotel
                 {
                     this.HotelsSet.Add(hotel);
+                }
+                this.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Méthodes Clients
+        public bool SupprimerClient(ClientsSet client)
+        {
+            try
+            {
+                var resultQuery = (from clients in this.ClientsSet
+                                   where clients.Id == client.Id
+                                   select clients);
+
+                if (resultQuery.Any())
+                {
+                    this.ClientsSet.Remove(resultQuery.First());
+                    this.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public ClientsSet GetClient(int idClient)
+        {
+            try
+            {
+                var resultQuery = (from clients in this.ClientsSet
+                                   where clients.Id == idClient
+                                   select clients);
+
+                if (resultQuery.Any())
+                {
+                    return resultQuery.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public bool ValiderClient(int idClient, string prenom, string nom, DateTime dateNaissance)
+        {
+            try
+            {
+                ClientsSet client;
+
+                if (idClient != 0) //si id!=0 alors l'hotel existe déjà
+                {
+                    client = GetClient(idClient);
+                }
+                else
+                {
+                    client = new ClientsSet();
+                }
+                client.Prenom = prenom;
+                client.Nom = nom;
+                client.DateNaissance = dateNaissance;
+
+                if (idClient == 0)   //nouvel hotel
+                {
+                    this.ClientsSet.Add(client);
+                }
+                this.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Méthodes Clients
+        public bool SupprimerChambre(ChambresSet chambre)
+        {
+            try
+            {
+                var resultQuery = (from chambres in this.ChambresSet
+                                   where chambres.Id == chambre.Id
+                                   select chambres);
+
+                if (resultQuery.Any())
+                {
+                    this.ChambresSet.Remove(resultQuery.First());
+                    this.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public ChambresSet GetChambre(int idChambre)
+        {
+            try
+            {
+                var resultQuery = (from chambres in this.ChambresSet
+                                   where chambres.Id == idChambre
+                                   select chambres);
+
+                if (resultQuery.Any())
+                {
+                    return resultQuery.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public bool ValiderChambre(int idChambre, string nom, bool climatisation, int nbLits, int keyHotel)
+        {
+            try
+            {
+                ChambresSet chambre;
+
+                if (idChambre != 0) //si id!=0 alors l'hotel existe déjà
+                {
+                    chambre = GetChambre(idChambre);
+                }
+                else
+                {
+                    chambre = new ChambresSet();
+                }
+                chambre.Nom = nom;
+                chambre.Climatisation = climatisation;
+                chambre.NbLits = nbLits;
+                chambre.KeyHotel = keyHotel;
+
+                if (idChambre == 0)   //nouvel hotel
+                {
+                    this.ChambresSet.Add(chambre);
+                }
+                this.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Méthodes Clients
+        public bool SupprimerReservation(ReservationSet reservation)
+        {
+            try
+            {
+                var resultQuery = (from reservations in this.ReservationsSet
+                                   where reservations.Id == reservation.Id
+                                   select reservations);
+
+                if (resultQuery.Any())
+                {
+                    this.ReservationsSet.Remove(resultQuery.First());
+                    this.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public ReservationSet GetReservation(int idReservation)
+        {
+            try
+            {
+                var resultQuery = (from reservations in this.ReservationsSet
+                                   where reservations.Id == idReservation
+                                   select reservations);
+
+                if (resultQuery.Any())
+                {
+                    return resultQuery.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public bool ValiderReservation(int idReservation, DateTime dateDebut, DateTime dateFin, int keyClient, int keyChambre)
+        {
+            try
+            {
+                ReservationSet reservation;
+
+                if (idReservation != 0) //si id!=0 alors l'hotel existe déjà
+                {
+                    reservation = GetReservation(idReservation);
+                }
+                else
+                {
+                    reservation = new ReservationSet();
+                }
+                reservation.DateDebut = dateDebut;
+                reservation.DateFin = dateFin;
+                reservation.KeyClient = keyClient;
+                reservation.KeyChambre = keyChambre;
+
+                if (idReservation == 0)   //nouvel hotel
+                {
+                    this.ReservationsSet.Add(reservation);
                 }
                 this.SaveChanges();
                 return true;
